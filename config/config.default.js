4e1422a61258bf31d019cfb7ea1e4bc07bc5049e
countryaccent <289486078@qq.com>
2017-05-09 22:27:55 +0800

    some add

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1494339549038_1922';
+
+  return config;
+};
